9cc22b90bc81faf6c0bd68d4624ef91945bc0158
Kamil Hryniewicz <kamil.hryniewicz@gmail.com>
2023-05-07 17:27:45 +0200

    Add 'Your question' label

--- searchgpt/app.py
+++ searchgpt/app.py
@@ -4,7 +4,7 @@ from searchgpt.llm import respond
 
 with gr.Blocks(title="SearchGPT") as demo:
     chatbot = gr.Chatbot(label="SearchGPT").style(height=400)
-    query = gr.Textbox(show_label=False)
+    query = gr.Textbox(label="Your question")
     query.submit(respond, [query, chatbot], [query, chatbot])
 
 demo.launch(share=False)
